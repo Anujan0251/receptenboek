92e9dd927a04134dcbf553e28306690b4c11d0fa
Anujan Partheepan <Vauki2004@gmail.com>
2023-03-24 13:25:06 +0100

    save

--- site/index.php
+++ site/index.php
@@ -1,5 +1,16 @@
 <?php
+require 'database.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
 
-// Dit is het startpunt van je applicatie.
-
-echo 'Welkom bij het receptenboek';
+</head>
+<body>
+    
+</body>
+</html>
