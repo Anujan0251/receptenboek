b89c3421883bc03f540168129108d7340a63701c
Anujan Partheepan <Vauki2004@gmail.com>
2023-03-28 11:36:52 +0200

    test

--- site/index.php
+++ site/index.php
@@ -1,74 +1,49 @@
 <?php
-require 'database.php';
-
+require "database.php";
 //de sql query
 $sql = "SELECT * FROM recepten";
-
 //hier wordt de query uitgevoerd met de database
 $result = mysqli_query($conn, $sql);
-
-/**
- * Hier wordt het resultaat ($result) omgezet
- * in een *multidimensionale associatieve array
- * in dit voorbeeld staat $all_users maar dit mag
- * voor bijvoorbeeld producten $all_products heten.
- * Maar dit kies je zelf
- */
 $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title></title>
-
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
 </head>
-
 <body>
     <header>
-
-        <div> <img class="logo" src="logo.jpg" /></div>
-        <nav>
-            <ul class="nav_links">
-                <li><a href="default.asp">Home</a></li>
-                <li><a href="news.asp">News</a></li>
-                <li><a href="contact.asp">Contact</a></li>
-                <li><a href="about.asp">About</a></li>
-            </ul>
+        <img class="logo" src="/images/logo.png" />
+        <nav> 
+          <ul class="nav__links">
+            <li><a href="index.php">home</a></li>
+            <li><a href="index.php">contact</a></li>
+            <li><a href="index.php">contact</a></li>
+          </ul>
         </nav>
-        </div>
-    </header>
-    <div class="recepten"
-    <?php foreach ($all_recepten as $recept) : ?>
-    < href="<?php echo $recept ['naam'] ?>.php" class="recept-card"> 
-        <div class="container">
-            <div class="plaatje">
-                <div class ="sgang">
-                    <p>Gang: <?php echo $recept['aantal personen']
-                </div>
-                <div class="spers">
+        <a class="cta" href="contact.php"><button>Contact</button></a>
+      </header> 
+      <img class="banner" src="/images/banner1.png" />
+<div class="flex-container">
 
+  <?php foreach ($all_recepten as $recept) : ?>
 
+    <div class="recept-blok">
 
+  <div class="naam">
+   <h3><?php echo $recept["naam"] ?></h3>
+  </div>
+  <img src="images/<?php echo $recept["foto"] ?>">
+</div>
 
+  <?php endforeach  ?>
 
-                <img src="images/<?php echo $recept['foto'] ?>">
-            </div>
-        </div>
-    <?php endforeach ?>
-
-
-
-
-
-
-
-    </div>
 </body>
 
 </html>
